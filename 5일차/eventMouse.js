44662781784517cba92b72c3bdd73e2907c159ab
SuCock <ksymin313@gamil.com>
2022-12-21 17:54:59 +0900

    5일차

---
+++ 5일차/eventMouse.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){ 
+  document.addEventListener("mousemove", function(ev){
+    // console.log(ev);
+    let img = document.querySelector("img");
+    img.style.left = ev.clientX + 'px';  //px는 단위
+    img.style.top = ev.clientY + 'px';
+  })
+})
