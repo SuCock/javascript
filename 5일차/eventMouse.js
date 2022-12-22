b796bd727e2d25a93aac70b6169d9e16ed943c25
SuCock <ksymin313@gamil.com>
2022-12-22 19:02:09 +0900

    보강

--- 5일차/eventMouse.js
+++ 5일차/eventMouse.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded",function(){ 
-  document.addEventListener("mousemove", function(ev){
-    // console.log(ev);
+  document.addEventListener("click", function(aa){
+    console.log(aa);
     let img = document.querySelector("img");
-    img.style.left = ev.clientX + 'px';  //px는 단위
-    img.style.top = ev.clientY + 'px';
+    img.style.left = aa.clientX + 'px';  //px는 단위
+    img.style.top = aa.clientY + 'px';   //mousemove의 function의 속성명.이벤트(이름을 안넣으면 event.이벤트)
   })
 })
